3bbb5bd5c2367b21628602181b265833bde80220
Sanaullah <xsanaullah.pk@gmail.com>
2026-01-01 14:57:51 +0500

    Added pass/fail logic

--- student_system/main.py
+++ student_system/main.py
@@ -3,5 +3,11 @@ print("Welcome to Student Result System")
 name = input("Enter student name: ")
 marks = int(input("Enter marks: "))
 
+if marks >= 40:
+    result = "PASS"
+else:
+    result = "FAIL"
+
 print("Student:", name)
 print("Marks:", marks)
+print("Result:", result)
